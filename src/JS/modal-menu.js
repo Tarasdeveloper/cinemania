7bc42f2a2d3f2e49ae1747f9a6658b3b2761db0c
OleksandrMol <raydar@ukr.net>
2023-06-05 14:57:18 +0300

    add localStorage
    сохраняется тема bg и  темы  хедера

--- src/JS/modal-menu.js
+++ src/JS/modal-menu.js
@@ -4,27 +4,45 @@ const backdrop = document.querySelector('.backdrop');
 const switchBt = document.querySelector('.switch');
 const logo = document.querySelector('.header__nav-text');
 const headerMenu = document.querySelector('.header__menu');
-   
+  
+
+window.addEventListener('DOMContentLoaded', () => {
+  try {
+    const isLightMode = localStorage.getItem('isLightMode');
+    const parsedLightMode = isLightMode ? JSON.parse(isLightMode) : null;
+    if (parsedLightMode !== null) {
+      document.body.classList.toggle('is-light-mode', parsedLightMode.body);
+      switchBt.classList.toggle('day', parsedLightMode.switchBtn);
+    }
+  } catch (err) {
+    console.error("Set state error: ", error.message);
+  }
+});
+
+switchBt.addEventListener('click', () => {
+  document.body.classList.toggle('is-light-mode');
+  const isLightMode = {
+    body: document.body.classList.contains('is-light-mode'),
+    switchBtn: switchBt.classList.contains('day'),
+  }
+  localStorage.setItem('isLightMode',JSON.stringify(isLightMode));
+});
+
+openMenuBtn.addEventListener('click', toggleMenu);
+backdrop.addEventListener('click', closeMenu);
+
+
 
 function toggleMenu () {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open-menu');
     
-    toggleModal();
-        
-        
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    toggleModal();      
 };
 
-openMenuBtn.addEventListener('click', toggleMenu);
-backdrop.addEventListener('click', closeMenu);
 
 function closeMenu(evt) {
-    console.log(evt)
     if (evt.target === mobileMenu || mobileMenu.contains(evt.target)) { 
         return }
     mobileMenu.classList.remove('is-open-menu');
@@ -38,23 +56,10 @@ function closeMenu(evt) {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open-menu');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
   });
 
 
 function toggleModal() {
     document.body.classList.toggle("is-open-menu");
     backdrop.classList.toggle('hidden-menu')
-}
-  
-function changeColor() {
-  if (!switchBt.classList.contains('day')) {
-    return
-  }
-  mobileMenu.style.backgroundColor = '#F8F8F8';
-  mobileMenu.style.color = '#111111';
-  openMenuBtn.style.color = '#595959';
-  logo.style.color = '#282828';
-  backdrop.style.backgroundColor ='rgba(0, 0, 0, 0.2)'
-  headerMenu.style.color = '#595959';
 }
