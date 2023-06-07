7e1a8b49809e39e75cac21c12477fa4c6dd1afd3
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-06-07 04:24:24 +0300

    fixed day/night button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './JS/api_key';
 import { openModalOops } from './JS/modal-oops';
 import { openModalPopUp } from './JS/modal-pop-up';
-import './JS/day_night_button';
 import './JS/day_night';
 import './JS/modal-menu';
 import './JS/hero';
