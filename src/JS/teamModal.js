655fb54e52e8cf568cadf6b6df00eb60d35aeb27
Nataliya Rieger <artgallery2018@gmx.ch>
2023-06-08 19:37:36 +0200

    update teamModal

--- src/JS/teamModal.js
+++ src/JS/teamModal.js
@@ -3,20 +3,20 @@ const refs = {
   closeModal: document.querySelector('.close-modal-team'),
   teamBackdrop: document.querySelector('.backdrop-modal'),
   teamModal: document.getElementsByClassName('team__modal'),
-  teamPhotos: document.querySelectorAll('.team__photo'), // Add this line
-  teamNames: document.querySelectorAll('.team__name') // Add this line
+  teamPhotos: document.querySelectorAll('.team__photo'), 
+  teamNames: document.querySelectorAll('.team__name')
 };
 
 if (refs.openModal) refs.openModal.addEventListener('click', openModalTeam);
 if (refs.closeModal) refs.closeModal.addEventListener('click', closeModalTeam);
 
-// Add event listeners for each team photo
+
 refs.teamPhotos.forEach(photo => {
   photo.addEventListener('mouseenter', handlePhotoHover);
   photo.addEventListener('mouseleave', handlePhotoHover);
 });
 
-// Add event listeners for each team name
+
 refs.teamNames.forEach(name => {
   name.addEventListener('mouseenter', handleNameHover);
   name.addEventListener('mouseleave', handleNameHover);
@@ -27,7 +27,6 @@ function openModalTeam(event) {
   document.addEventListener('keydown', onEscapeClose);
   document.addEventListener('click', onBackdropClose);
   refs.teamModal[0].classList.add('openModalAnimationTeam');
-  // Disable page scroll
   document.body.style.overflow = 'hidden';
 }
 
@@ -35,7 +34,6 @@ function closeModalTeam(event) {
   refs.teamModal[0].classList.remove('closeModalAnimationTeam');
   refs.teamBackdrop.classList.add('team__backdrop--hidden');
   document.removeEventListener('keydown', onEscapeClose);
-  // Enable page scroll
   document.body.style.overflow = '';
 }
 
