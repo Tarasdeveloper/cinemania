425a287fc3be5f18b909b46568228efd13b14e82
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-06-09 02:39:44 +0300

    replaced button with 'X' into button with close-svg, added styles for this btn and scroll blocking while modal-oops is opened, but for modal-oops-details

--- src/JS/modal-oops-details.js
+++ src/JS/modal-oops-details.js
@@ -4,14 +4,17 @@ export function openModalOopsDetails() {
     '.modal-oops-details-close-btn'
   );
   modalOopsDetails.style.display = 'flex';
+  document.body.classList.add('modal-open');
   modalOopsDetailsCloseBtn.addEventListener('click', function () {
     modalOopsDetails.style.display = 'none';
+    document.body.classList.remove('modal-open');
   });
   window.addEventListener('click', onOopsDetailsBackdropClick);
   function onOopsDetailsBackdropClick(event) {
     if (event.target == modalOopsDetails) {
       modalOopsDetails.style.display = 'none';
       window.removeEventListener('click', onEscPress, false);
+      document.body.classList.remove('modal-open');
     }
   }
   document.addEventListener('keydown', onEscPress);
@@ -20,6 +23,7 @@ export function openModalOopsDetails() {
     if (event.code === ESC_KEYCODE) {
       modalOopsDetails.style.display = 'none';
       document.removeEventListener('keydown', onEscPress, false);
+      document.body.classList.remove('modal-open');
     }
   }
 }
